1d1f58002f9ece013a96a2bbfce5df50da5f5d6f
ekjoy <eldhosekjoy@outlook.com>
2024-01-25 22:01:47 +0000

    Project name update

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,49 +1,67 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import {Course} from '../model/course';
-import {CoursesService} from '../services/courses.service';
-import {debounceTime, distinctUntilChanged, startWith, tap, delay} from 'rxjs/operators';
-import {merge, fromEvent} from 'rxjs';
-import {Lesson} from '../model/lesson';
-import {Meta, Title} from '@angular/platform-browser';
-
+import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  startWith,
+  tap,
+  delay,
+} from 'rxjs/operators';
+import { merge, fromEvent } from 'rxjs';
+import { Lesson } from '../model/lesson';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
-    selector: 'course',
-    templateUrl: './course.component.html',
-    styleUrls: ['./course.component.scss']
+  selector: 'course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.scss'],
 })
 export class CourseComponent implements OnInit {
+  course: Course;
 
+  dataSource: MatTableDataSource<Lesson>;
 
-    course:Course;
+  displayedColumns = ['seqNo', 'description', 'duration'];
 
-    dataSource: MatTableDataSource<Lesson>;
+  constructor(
+    private route: ActivatedRoute,
+    private coursesService: CoursesService,
+    private title: Title,
+    private meta: Meta
+  ) {}
 
-    displayedColumns= ["seqNo", "description", "duration"];
+  ngOnInit() {
+    this.course = this.route.snapshot.data['course'];
 
+    this.dataSource = new MatTableDataSource([]);
 
-    constructor(
-        private route: ActivatedRoute,
-        private coursesService: CoursesService) {
+    this.coursesService
+      .findAllCourseLessons(this.course.id)
+      .subscribe((lessons) => (this.dataSource.data = lessons));
 
-    }
+    this.title.setTitle(this.course.description);
 
+    this.meta.updateTag({
+      name: 'description',
+      content: this.course.longDescription,
+    });
 
-
-    ngOnInit() {
-
-        this.course = this.route.snapshot.data["course"];
-
-        this.dataSource = new MatTableDataSource([]);
-
-        this.coursesService.findAllCourseLessons(this.course.id)
-            .subscribe(lessons => this.dataSource.data = lessons);
-
-    }
-
-
+    this.meta.updateTag({ name: 'og:image', content: this.course.iconUrl });
+    this.meta.updateTag({
+      name: 'og:description',
+      content: this.course.description,
+    });
+    this.meta.updateTag({ name: 'og:title', content: this.course.description });
+  }
 }
